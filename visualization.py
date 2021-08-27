e5db23e2ac95d8e707e4c5d59817f223704d6944
Ahmad Qadri <arqchicago@gmail.com>
2021-08-26 22:48:53 -0400

    visualization class that creates beautiful plots

---
+++ visualization.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+
+
+class visualization:
+    def __init__(self):
+        self.text_watermark = ''
+        self.text_plot_title = ''
+        self.text_y_label = ''
+        self.text_x_label = ''
+        
+    @property
+    def y_label(self):
+        return self.text_y_label
+        
+    @y_label.setter
+    def y_label(self, text_y_label):
+        return self.text_y_label
+        
+    @property
+    def x_label(self):
+        return self.text_x_label
+        
+    @x_label.setter
+    def x_label(self, text_x_label):
+        return self.text_x_label
+        
+    @property
+    def plot_title(self):
+        return self.text_plot_title
+        
+    @plot_title.setter
+    def plot_title(self, text_plot_title):
+        return self.text_plot_title
+        
+    @property
+    def watermark(self):
+        return self.text_watermark
+        
+    @plot_title.setter
+    def watermark(self, text_watermark):
+        return self.text_watermark
+
+    def plot(self, fig_name):
+        mpl.style.use('seaborn')
+        plt.plot(df['x'], df['y'], color='b', label='test')
+        plt.title(self.text_plot_title)
+        subtitle = 'this is the subtitle'
+        plt.suptitle(subtitle, fontsize=10)
+        plt.ylabel(self.text_y_label)
+        plt.xlabel(self.text_x_label)
+        plt.gca().set_xlim(left=0)
+        plt.gca().set_ylim(bottom=0)
+        #plt.text(10, 20, self.text_watermark, font=40, color='red', ha='right', va='bottom', alpha=0.25, rotation=40)
+        plt.legend()
+        plt.savefig(fig_name)
+        plt.close()
+        
+        
+if __name__ == "__main__":
+    df = pd.read_csv('data/test.csv')
+    plot1 = visualization()
+    plot1.plot_title = 'Plot of Y vs X'
+    plot1.y_label = 'Y'
+    plot1.x_label = 'X'
+    plot1.watermark = 'do not distribute'
+    plot1.plot('fig1.png')
