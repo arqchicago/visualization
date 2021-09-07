268077ec69c0c71d679f0e8208219b4cb4dc97bc
Ahmad Qadri <arqchicago@gmail.com>
2021-09-07 19:41:04 -0400

    switching to calling axes/subplots

--- visualization.py
+++ visualization.py
@@ -10,7 +10,8 @@ class visualization:
         self.text_plot_title = ''
         self.text_y_label = ''
         self.text_x_label = ''
-        
+        self.fig, self.ax = plt.subplots()
+
     @property
     def y_label(self):
         return self.text_y_label
@@ -43,27 +44,22 @@ class visualization:
     def watermark(self, text):
         self.text_watermark = text
 
-    def plot(self, fig_name):
+    def line_chart(self, fig_name):
         mpl.style.use('seaborn')
-        plt.plot(df['x'], df['y'], color='b', label='test')
-        plt.title(self.text_plot_title)
-        subtitle = 'this is the subtitle'
-        plt.suptitle(subtitle, fontsize=10)
-        plt.ylabel(self.text_y_label)
-        plt.xlabel(self.text_x_label)
-        plt.gca().set_xlim(left=0)
-        plt.gca().set_ylim(bottom=0)
-        #plt.text(10, 20, self.text_watermark, font=40, color='red', ha='right', va='bottom', alpha=0.25, rotation=40)
-        plt.legend()
+        self.ax.plot(df['x'], df['y'], color='b', label='test')
+        self.ax.set(title=self.text_plot_title)
+        self.ax.set_xlabel(self.text_x_label)
+        self.ax.set_ylabel(self.text_y_label)
+        self.ax.legend()
         plt.savefig(fig_name)
         plt.close()
-        
+
         
 if __name__ == "__main__":
     df = pd.read_csv('data/test.csv')
-    plot1 = visualization()
-    plot1.plot_title = 'Plot of Y vs X'
-    plot1.y_label = 'Y'
-    plot1.x_label = 'X'
-    plot1.watermark = 'do not distribute'
-    plot1.plot('fig1.png')
+    line_chart1 = visualization()
+    line_chart1.plot_title = 'Plot of Y vs X'
+    line_chart1.y_label = 'Y'
+    line_chart1.x_label = 'X'
+    #line_chart1.watermark = 'do not distribute'
+    line_chart1.line_chart('fig1.png')
