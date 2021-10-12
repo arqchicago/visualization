d83e468c1e19094b296a9042fad3464993ea5482
Ahmad Qadri <arqchicago@gmail.com>
2021-10-12 19:59:37 -0400

    added a scatterplot

--- visualization.py
+++ visualization.py
@@ -72,6 +72,25 @@ class visualization:
         plt.savefig(fig_name)
         plt.close()
 
+    def scatter(self, fig_name):
+        self.fig, self.ax = plt.subplots()
+        mpl.style.use('seaborn')
+
+        self.fig.suptitle(self.text_plot_suptitle, fontsize=16)
+
+        for axis in ['bottom','left']:  #'top', 'right'
+            self.ax.spines[axis].set_linewidth(2)
+            self.ax.spines[axis].set_color('red')
+          
+        self.ax.scatter(self.df['x'], self.df['y'], color='b', label='test')
+        self.ax.set(title=self.text_plot_title)
+        self.ax.set_xlabel(self.text_x_label)
+        self.ax.set_ylabel(self.text_y_label)
+        self.ax.legend()
+        self.fig.text(0.85, 0.15, self.text_watermark, fontsize=25, color='gray', ha='right', va='bottom', alpha=0.5)
+        plt.savefig(fig_name)
+        plt.close()
+
         
 if __name__ == "__main__":
     df = pd.read_csv('data/test.csv')
@@ -82,3 +101,4 @@ if __name__ == "__main__":
     data_viz.x_label = 'X'
     data_viz.watermark = 'do not distribute'
     data_viz.line_chart('fig1.png')
+    data_viz.scatter('fig2.png')
